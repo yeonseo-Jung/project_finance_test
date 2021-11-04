3a53f07ab1aca4eb96d2dd89d1f2afbbad891d8a
yeonseo <yeonseosla@gmail.com>
2021-11-05 03:40:07 +0900

    2021-11-05

--- finstate_fnguide_crawling.py
+++ finstate_fnguide_crawling.py
@@ -76,6 +76,7 @@ def get_finstate_highlight_annual(stock_code):
 
 
 # 단일회사 연간, 분기 재무제표(cis, bs, cfs) 크롤링 함수 
+# 단일회사 연간 재무제표 주요계정 및 재무비율 크롤링 함수 
 def get_finstate(stock_code, finstate):
     
     finstate_dict = {
@@ -87,8 +88,7 @@ def get_finstate(stock_code, finstate):
         "cfs_q": "divCashQ",
     }
     finstate_kind = finstate_dict[finstate]
-
-    ''' 경로 탐색'''
+    
     # 경로 탐색 
     url = re.get(f'https://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode=A{stock_code}')
     url = url.content
@@ -102,19 +102,31 @@ def get_finstate(stock_code, finstate):
     tbody = table.find('tbody')
     tr = tbody.find_all('tr')
 
-    Table = pd.DataFrame()
+    # 기간 가져오기 
+    # table columns 지정
+    thead = table.find("thead")
+    terms = thead.find("tr").find_all("th")
 
+    columns_arr = []
+    for q in terms:
+        columns_arr.append(q.text)    
+    columns_arr[0] = "Account"
+    # columns 지정
+    Table = pd.DataFrame(columns=columns_arr)
+    
+    index = 0
     for i in tr:
 
-        ''' 항목 가져오기'''
-        category = i.find('span',{'class':'txt_acd'})
+        # 계정과목이름 
+        account_nm = i.find('span',{'class':'txt_acd'})
 
-        if category == None:
-            category = i.find('th')   
+        if account_nm == None:
+            account_nm = i.find('th')   
 
-        category = category.text.strip()
+        account_nm = account_nm.text.strip()
+        Table.loc[index, "Account"] = account_nm
 
-        '''값 가져오기'''
+        # 금액 
         value_list =[]
 
         values = i.find_all('td',{'class':'r'})
@@ -128,22 +140,8 @@ def get_finstate(stock_code, finstate):
             except:
                 value_list.append(0)
 
-        Table['%s'%(category)] = value_list
+        Table.loc[index, columns_arr[1:]] = value_list
         
-
-        ''' 기간 가져오기 '''    
-
-        # columns(최근 4개 분기) 
-
-        thead = table.find("thead")
-        quarters = thead.find("tr").find_all("th")
-
-        quarter_arr = []
-        for q in quarters[1:]:
-            quarter_arr.append(q.text)
-
-        Table.index = quarter_arr
-
-    Table = Table.T
-
+        index += 1
+        
     return Table
